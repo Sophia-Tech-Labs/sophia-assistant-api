03ca708f36121d29a954614233360dca2b698745
ayanokojix23 <ayanokojix2306@gmail.com>
2025-06-08 07:25:01 +0000

    Added Post login route

--- routes/superAdminRoutes.js
+++ routes/superAdminRoutes.js
@@ -2,5 +2,5 @@ const express = require("express");
 const router = express.Router();
 const supAdmFn  = require("../controllers/superAdminController");
 router.post("/signup",supAdmFn.superAdminSignup);
-
+router.post("/login",supAdmFn.superAdminLogin);
 module.exports = router;
