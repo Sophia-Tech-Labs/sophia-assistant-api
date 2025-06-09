0e112f955e611edd97ab5c309255f90f85c47d17
ayanokojix23 <ayanokojix2306@gmail.com>
2025-06-09 15:20:54 +0000

    .

--- test.js
+++
@@ -1,6 +0,0 @@
-const db = require("./db/db.js");
-const email = "ayanokoji@gmail.com";
-(async()=>{
-	const output = await db.query("SELECT * FROM super_admins WHERE email = $1",[email]);
-	await console.log(output)
-})()
