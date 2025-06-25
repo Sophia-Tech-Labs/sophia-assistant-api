624240c66286d6b9b67978353e59a3e434db7e11
Ayanokoji kiyotaka <ayanokojix2306@gmail.com>
2025-06-25 12:18:03 +0100

    Update superAdminRoutes.js

--- routes/superAdminRoutes.js
+++ routes/superAdminRoutes.js
@@ -15,18 +15,7 @@ router.post("/forgot-password",loginLimiter,superAdminForgotPassword);
 router.post("/reset-password",loginLimiter,superAdminResetPassword);
 router.post("/login",loginLimiter,supAdmFn.superAdminLogin);
 router.post("/invite-admin",superAdminMd.verifySuperAdmin,supAdmFn.inviteAdmin);
-<<<<<<< HEAD
-router.get("/protect",superAdminMd.verifySuperAdmin,(req,res)=>{
-	res.send(`
-	<body>
-	<h1>hello, ${req.user.name} </h1>
-	<h1> You are a ${req.user.role} </h1>  
-</body>
-	`)
-})
-=======
 router.delete("/remove-admin/:id",superAdminMd.verifySuperAdmin,supAdmFn.removeAdmin);
 router.get("/view-all-admins",superAdminMd.verifySuperAdmin,supAdmFn.viewAllAdmins);
 router.get("/view-one-admin/:id",superAdminMd.verifySuperAdmin,supAdmFn.viewOneAdmin);
->>>>>>> 69e5035ca82d832ba2413902a94ecb32c2b772d3
 module.exports = router;
